0b2d812dfa99d4f6a8000c2406f2227b41c131a3
Anushka Gangal <anushka8197@gmail.com>
2025-03-05 04:47:59 +0000

    Updated root URL

--- service/routes.py
+++ service/routes.py
@@ -44,7 +44,11 @@ def health_check():
 def index():
     """Root URL response"""
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Wishlist REST API Service",
+            version="1.0",
+            paths=url_for("list_wishlists", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
