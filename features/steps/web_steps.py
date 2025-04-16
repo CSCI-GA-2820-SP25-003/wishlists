2273e5a7bf7cc8560da7038e57c06981d9d80b2a
Damini Kaushik <daminieq@gmail.com>
2025-04-16 02:52:46 +0000

    create wishlists code

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -1,8 +1,13 @@
 # pylint: disable=function-redefined, missing-function-docstring
 # flake8: noqa
 import os
+from typing import Any
+import re
 import requests
 from behave import given, when, then  # pylint: disable=no-name-in-module
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 # HTTP Return Codes
 HTTP_200_OK = 200
@@ -10,6 +15,22 @@ HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
 
 WAIT_TIMEOUT = 60
+
+ID_PREFIX = "wishlist_"
+
+
+def save_screenshot(context: Any, filename: str) -> None:
+    """Takes a snapshot of the web page for debugging and validation
+
+    Args:
+        context (Any): The session context
+        filename (str): The message that you are looking for
+    """
+    # Remove all non-word characters (everything except numbers and letters)
+    filename = re.sub(r"[^\w\s]", "", filename)
+    # Replace all runs of whitespace with a single dash
+    filename = re.sub(r"\s+", "-", filename)
+    context.driver.save_screenshot(f"./captures/{filename}.png")
 
 
 @given("the server is started")
@@ -19,13 +40,35 @@ def step_impl(context):
     assert context.resp.status_code == 200
 
 
-@when('I visit the "home page"')
-def step_impl(context):
-    context.resp = requests.get(context.base_url + "/", timeout=WAIT_TIMEOUT)
-    assert context.resp.status_code == 200
+@when('I visit the "Home Page"')
+def step_impl(context: Any) -> None:
+    """Make a call to the base URL"""
+    context.driver.get(context.base_url)
 
 
 @then('I should see "{message}" And I should not see "{text_string}"')
 def step_impl(context, message, text_string):
     assert message in str(context.resp.text)
     assert text_string not in str(context.resp.text)
+
+
+@when('I set the "{element_name}" to "{text_string}"')
+def step_impl(context: Any, element_name: str, text_string: str) -> None:
+    element_id = ID_PREFIX + element_name.lower().replace(" ", "_")
+    element = context.driver.find_element(By.ID, element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+
+@when('I select "{text}" in the "{element_name}" dropdown')
+def step_impl(context: Any, text: str, element_name: str) -> None:
+    element_id = ID_PREFIX + element_name.lower().replace(" ", "_")
+    element = Select(context.driver.find_element(By.ID, element_id))
+    element.select_by_visible_text(text)
+
+
+@then('I should see "{text}" in the "{element_name}" dropdown')
+def step_impl(context: Any, text: str, element_name: str) -> None:
+    element_id = ID_PREFIX + element_name.lower().replace(" ", "_")
+    element = Select(context.driver.find_element(By.ID, element_id))
+    assert element.first_selected_option.text == text
--- features/wishlists.feature
+++ features/wishlists.feature
@@ -7,5 +7,24 @@ Background:
   Given the server is started
 
 Scenario: The server is running
-  When I visit the "home page"
-  Then I should see "Wishlist REST API Service" And I should not see "404 Not Found"
+  When I visit the "Home Page"
+  Then I should see "Wishlist Demo REST API Service" And I should not see "404 Not Found"
+
+Scenario: Create a Wishlist
+    When I visit the "Home Page"
+    # And I set the "Name" to "Wishlist 1"
+    # And I set the "User ID" to "1"
+    # And I select "" in the "Products" dropdown
+    # And I press the "Create" button
+    # Then I should see the message "Success"
+    # When I copy the "Wishlist ID" field
+    # And I press the "Clear" button
+    # Then the "User ID" field should be empty
+    # And the "Name" field should be empty
+    # And the "Products" field should be empty
+    # When I paste the "Wishlist ID" field
+    # And I press the "Retrieve" button
+    # Then I should see the message "Success"
+    # And I should see "Wishlist 1" in the "Name" field
+    # And I should see "1" in the "User ID" field
+    # And I should see "" in the "Products" dropdown
