7e47d4582fa7166521484ef33e9ccf5b5e97691b
Damini Kaushik <daminieq@gmail.com>
2025-04-16 20:00:44 +0000

    added update product feature

--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -15,14 +15,13 @@
 ######################################################################
 
 """
-Wishlist Steps
+Product Steps
 
-Steps file for Wishlist.feature
+Steps file for Product.feature
 
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
-import ast
 import requests
 from compare3 import expect
 from behave import given  # pylint: disable=no-name-in-module
@@ -35,31 +34,38 @@ HTTP_204_NO_CONTENT = 204
 WAIT_TIMEOUT = 60
 
 
-@given("the following wishlists")
+@given("the following products")
 def step_impl(context):
-    """Delete all Wishlists and load new ones"""
+    """Delete all Products and load new ones"""
 
-    # Get a list all of the wishlists
-    rest_endpoint = f"{context.base_url}/wishlists"
+    # Get a list all of the products
+    wishlist_id = context.wishlist_ids[0]
+    # print("All wishlist IDs:", context.wishlist_ids)
+    # print("Posting to wishlist:", wishlist_id)
+    rest_endpoint = f"{context.base_url}/wishlists/{wishlist_id}/products"
     context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
     expect(context.resp.status_code).equal_to(HTTP_200_OK)
     # and delete them one by one
-    context.wishlist_ids = []
-    for wishlist in context.resp.json():
+    for product in context.resp.json():
         context.resp = requests.delete(
-            f"{rest_endpoint}/{wishlist['id']}", timeout=WAIT_TIMEOUT
+            f"{rest_endpoint}/{product['id']}", timeout=WAIT_TIMEOUT
         )
         expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
 
-    # load the database with new wishlists
+    # load the database with new products
     for row in context.table:
-        products = ast.literal_eval(row["products"]) if row["products"] else []
         payload = {
             "name": row["name"],
-            "userid": row["userid"],
-            "products": products,
+            "wishlist_id": wishlist_id,
+            "price": float(row["price"]),  # for Decimal(10,2), float is acceptable
+            # "min_price": int(row["min_price"]),  # if your schema uses this — remove if not
+            # "max_price": int(row["max_price"]),  # same here
+            "quantity": int(row["quantity"]),
+            "description": row["description"],
+            "note": row["note"],
+            "is_gift": row["is_gift"].lower() == "true",
+            "purchased": row["purchased"].lower() == "true",
         }
         context.resp = requests.post(rest_endpoint, json=payload, timeout=WAIT_TIMEOUT)
+        # print("Response Body:", context.resp.text)
         expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
-        wishlist = context.resp.json()
-        context.wishlist_ids.append(wishlist["id"])
