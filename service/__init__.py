82b90d8536bd0c7291adffecccd9b853138c0618
Anushka Gangal <anushka8197@gmail.com>
2025-04-30 04:29:56 +0000

    fixed style issue

--- service/__init__.py
+++ service/__init__.py
@@ -30,7 +30,7 @@ from service.common import log_handlers
 def create_app():
     """Initialize the core application."""
     # Create Flask application
-    app = Flask(__name__, static_folder="service/static", static_url_path="/")
+    app = Flask(__name__)
     app.config.from_object(config)
 
     # Initialize Plugins
